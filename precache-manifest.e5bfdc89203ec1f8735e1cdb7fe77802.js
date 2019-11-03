e39b60802da4497b9039bdf3ce13ac656ece723e
Sellieee <Sellieee@hotmail.com>
2019-11-03 15:36:00 +1100

    Updates

---
+++ precache-manifest.e5bfdc89203ec1f8735e1cdb7fe77802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308c9970efffce566027c0e5fa1b3154",
+    "url": "/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "02a50db723831b8b156d",
+    "url": "/React-Clicky-Game/static/css/main.8612855e.chunk.css"
+  },
+  {
+    "revision": "8ddf23b8ae8e7f5c8847",
+    "url": "/React-Clicky-Game/static/js/2.19c06209.chunk.js"
+  },
+  {
+    "revision": "02a50db723831b8b156d",
+    "url": "/React-Clicky-Game/static/js/main.0f56189f.chunk.js"
+  },
+  {
+    "revision": "40f6f9a36f6865ad2c6a",
+    "url": "/React-Clicky-Game/static/js/runtime-main.f426ea82.js"
+  }
+]);
